d580ae176e9b3dd25656e9f323fe3fd21935dd0a
andres magnago <114195498+AndresMagnago@users.noreply.github.com>
2025-11-06 11:51:23 -0300

    Add files via upload

---
+++ Conexion.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+package com.todosport;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Conexion {
+    private static final String URL = "jdbc:mysql://localhost:3306/todosport";
+    private static final String USER = "root";
+    private static final String PASSWORD = "";
+
+    public static Connection getConnection() {
+        Connection conn = null;
+        try {
+            conn = DriverManager.getConnection(URL, USER, PASSWORD);
+            System.out.println("✅ Conexión exitosa a la base de datos.");
+        } catch (SQLException e) {
+            System.out.println("❌ Error al conectar: " + e.getMessage());
+        }
+        return conn;
+    }
+}
